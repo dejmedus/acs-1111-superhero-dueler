3f0ac7b8c1307c237c32028f259faeec839acb4f
Julia <email96123@gmail.com>
2024-11-16 13:15:47 -0700

    added abilities and armor

--- hero.py
+++ hero.py
@@ -4,19 +4,15 @@ import random
 class Hero:
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
-    def __init__(self, name, starting_health=100):
+    def __init__(self, name,  starting_health=100):
         '''Instance properties:
           name: String
+
           starting_health: Integer
           current_health: Integer
         '''
-
-        # we know the name of our hero, so we assign it here
         self.name = name
-        # similarly, our starting health is passed in, just like name
         self.starting_health = starting_health
-        # when a hero is created, their current health is
-        # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
 
     def fight(self, opponent):
@@ -34,5 +30,4 @@ if __name__ == "__main__":
     # this block is executed.
     hero1 = Hero("Wonder Woman", 300)
     hero2 = Hero("Dumbledore", 250)
-
     hero1.fight(hero2)
